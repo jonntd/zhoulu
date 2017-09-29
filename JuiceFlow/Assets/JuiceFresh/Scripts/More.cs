eb5be2c0abb26856ffd4b129e3afb207fbf49f40
CodeF <funcodegroup@gmail.com>
2017-09-30 00:11:22 +0800

    1.修改UI布局 2.替换背景音乐

---
+++ JuiceFlow/Assets/JuiceFresh/Scripts/More.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class More : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void MoreGame()
+    {
+        #if UNITY_ANDROID
+        Application.OpenURL("https://play.google.com/store/apps/dev?id=6793500223563275059");
+        #elif UNITY_IPHONE
+        Application.OpenURL("https://itunes.apple.com/developer/xiaojie-wang/id1040525394");
+        #endif  
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
