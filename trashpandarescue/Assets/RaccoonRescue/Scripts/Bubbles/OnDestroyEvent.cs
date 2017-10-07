30689a1077400cd1260ccfe08ffe8327ebf9f757
CodeF <funcodegroup@gmail.com>
2017-10-07 18:35:13 +0800

    新建小浣熊泡泡龙

---
+++ trashpandarescue/Assets/RaccoonRescue/Scripts/Bubbles/OnDestroyEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnDestroyEvent : MonoBehaviour {
+	public GameObject[] instantiatePrefabs;
+
+	void OnDisable () {
+		if (!isQuitting) {
+			foreach (GameObject item in instantiatePrefabs) {
+				Instantiate (item, transform.position, transform.rotation);
+			}
+		}
+	}
+
+	bool isQuitting;
+
+	void OnApplicationQuit () {
+		isQuitting = true;
+	}
+}
+
