c24a680375c1058caf6bdc3497f63cefef272c78
mashaomin <m623606643@163.com>
2017-10-24 20:37:38 +0800

    update

---
+++ Msm_Match/Assets/Editor_Match/Editor/map_editor/EditorMapLevel.cs
@@ -0,0 +1,32 @@
+﻿
+using SummerEditor;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace Summer.Editor
+{
+    public class EditorMapLevel : EditorWindow
+    {
+        static float t_width = 1280;
+        static float t_height = 720;
+        public EComponent _container;
+
+        public static void ShowWindown()
+        {
+            EditorMapLevel win = EditorWindow.GetWindow<EditorMapLevel>();
+            win.minSize = new Vector2(t_width, t_height);
+            win.maxSize = new Vector2(t_width + 40, t_height + 40);
+            win.Show();
+        }
+
+        public EditorMapLevel()
+        {
+            _container = new EComponent(t_width, t_height);
+            _container.show_bg = true;
+            _container.ResetPosition(t_width / 2, t_height / 2);
+            _container.SetBg(0, 0, 0, 1);
+        }
+    }
+}
+
