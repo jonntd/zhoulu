ef25ca8c871bc040324a464c4b5751152ae0c095
CodeF <funcodegroup@gmail.com>
2017-11-02 22:40:28 +0800

    修改umengID

--- IceBlast/Assets/Scripts/summer_statistics/StatisticsMgr.cs
+++ IceBlast/Assets/Scripts/summer_statistics/StatisticsMgr.cs
@@ -17,12 +17,12 @@ namespace com.summer
             //StatisticsManager.AppKeyAndChannelId("59c5fe4b07fe65541e000047", "Android");
 
 #if UNITY_ANDROID
-            StatisticsManager.AppKeyAndChannelId("59c5fe4b07fe65541e000047", "Googleplay");
+            StatisticsManager.AppKeyAndChannelId("59da31f9734be42d1500000f", "Googleplay");
             //调试时开启日志 发布时设置为false
             StatisticsManager.SetLogEnabled(false);
             //StatisticsManager.SetLogEnabled (true);	
 #elif UNITY_IPHONE
-            StatisticsManager.AppKeyAndChannelId("59c5fef14ad1567064000072", "AppStore");
+            StatisticsManager.AppKeyAndChannelId("59da3251c62dca29c1000339", "AppStore");
 		    //调试时开启日志 发布时设置为false
 		    StatisticsManager.SetLogEnabled (false);
 		    //GA.SetLogEnabled (true);	
