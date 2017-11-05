b1bc5b761c067d0348ba58ea1878b9e0cf137a60
CodeF <funcodegroup@gmail.com>
2017-11-05 20:26:28 +0800

    修改prefab配置

--- IceBlast/Assets/Scripts/PanelSetting.cs
+++ IceBlast/Assets/Scripts/PanelSetting.cs
@@ -23,6 +23,15 @@ public class PanelSetting : MonoBehaviour
                 Application.OpenURL("https://itunes.apple.com/developer/xiaojie-wang/id1040525394");
         #endif
     }
+    public void on_rate()
+    {
+#if UNITY_ANDROID
+        Application.OpenURL("https://play.google.com/store/apps/details?id=com.codef.iceblast");
+#elif UNITY_IPHONE
+                Application.OpenURL("https://itunes.apple.com/app/ice-blast-jewel-scatter/id1296636369");
+#endif
+    }
+
     // Update is called once per frame
     void Update () {
 		
