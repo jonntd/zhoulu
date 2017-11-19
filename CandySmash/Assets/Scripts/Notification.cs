d503c6683a2008cb685e3db811ebfa24e2faceb4
CodeF <funcodegroup@gmail.com>
2017-11-19 13:51:50 +0800

    1.修改Notice；2.增加地图资源

--- CandySmash/Assets/Scripts/Notification.cs
+++ CandySmash/Assets/Scripts/Notification.cs
@@ -1,50 +1,135 @@
-﻿using System;
+﻿#if UNITY_ANDROID
+using System;
 using UnityEngine;
+using Assets.SimpleAndroidNotifications;
 
-#if UNITY_ANDROID
-namespace Assets.SimpleAndroidNotifications
+public class Notification : MonoBehaviour
 {
-    public class Notification : MonoBehaviour
+    void Awake()
     {
-        public string title;
-        public string message;
-        public string ticker;
+        //第一次进入游戏的时候清空，有可能用户自己把游戏冲后台杀死，这里强制清空
+        CleanNotification();
+    }
 
-        void Start()
+    /*
+        void OnEnable()
         {
+            Debug.Log("OnEnable");
+            CleanNotification();
+        }
 
-        //Debug.Log("消息推送");
-        var notificationParams = new NotificationParams
+        void OnGUI()
+        {
+            if (GUILayout.Button("5 SECONDS", GUILayout.Height(Screen.height * 0.2f)))
             {
-                Id = UnityEngine.Random.Range(0, int.MaxValue),
-                Delay = TimeSpan.FromSeconds(172800),
-                //Delay = TimeSpan.FromSeconds(17),
-                Title = "Got Free Now?",
-				//标题
-                Message = "Come back to Candy Smash and enjoy the fun time!",
-                //文本内容
-				Ticker = "Candy Smash",
-				//缩略内容
-                Sound = true,
-                Vibrate = true,
-                Light = true,
-                SmallIcon = NotificationIcon.Heart,
-                SmallIconColor = new Color(0, 0.5f, 0),
-                LargeIcon = "app_icon"
-            };
-
-            //此处开始是保证只发送1次推送的处理
-            var SendNotification = GameObject.Find("SendNotificationObj");
-            if (SendNotification == null)
-            {
-                SendNotification = new GameObject();
-                SendNotification.name = "SendNotificationObj";
-                NotificationManager.SendCustom(notificationParams);
-                DontDestroyOnLoad(SendNotification);
-                //Debug.Log("消息推送-消息发送");
+                NotificationManager.Send(TimeSpan.FromSeconds(NotificationConfig.day3_time), "title", NotificationConfig.day3_text, new Color(1, 0.3f, 0.15f));
             }
-
+        }
+    */
+    void OnApplicationPause(bool paused)
+    {
+        Debug.Log("OnApplicationPause" + paused);
+        //程序进入后台时
+        if (paused)
+        {
+            NotificationManager.Send(TimeSpan.FromSeconds(NotificationConfig.day3_time), NotificationConfig.NoticeTitle, NotificationConfig.day3_text, new Color(1, 0.3f, 0.15f));
+            NotificationManager.Send(TimeSpan.FromSeconds(NotificationConfig.day7_time), NotificationConfig.NoticeTitle, NotificationConfig.day7_text, new Color(1, 0.3f, 0.15f));
+            NotificationManager.Send(TimeSpan.FromSeconds(NotificationConfig.life_full_time), NotificationConfig.NoticeTitle, NotificationConfig.lift_full, new Color(1, 0.3f, 0.15f));
+            NotificationManager.Send(TimeSpan.FromSeconds(NotificationConfig.next_day_time), NotificationConfig.NoticeTitle, NotificationConfig.next_day_text, new Color(1, 0.3f, 0.15f));
+        }
+        else
+        {
+            //程序从后台进入前台时
+            CleanNotification();
         }
     }
-}			
+
+    //清空所有本地消息
+    void CleanNotification()
+    {
+        NotificationManager.CancelAll();
+    }
+}
+#elif UNITY_IPHONE
+
+using UnityEngine;
+using System.Collections;
+
+using NotificationServices = UnityEngine.iOS.NotificationServices;
+using NotificationType = UnityEngine.iOS.NotificationType;
+
+public class Notification : MonoBehaviour {
+
+//本地推送
+public static void NotificationMessage(string message,int hour ,bool isRepeatDay)
+{
+int year = System.DateTime.Now.Year;
+int month = System.DateTime.Now.Month;
+int day= System.DateTime.Now.Day;
+System.DateTime newDate = new System.DateTime(year,month,day,hour,0,0);
+NotificationMessage(message,newDate,isRepeatDay);
+}
+
+//本地推送 你可以传入一个固定的推送时间
+public static void NotificationMessage(string message,System.DateTime newDate,bool isRepeatDay)
+{
+//推送时间需要大于当前时间
+if(newDate > System.DateTime.Now)
+{
+UnityEngine.iOS.LocalNotification localNotification = new UnityEngine.iOS.LocalNotification();
+localNotification.fireDate = newDate;	
+localNotification.alertBody = message;
+localNotification.applicationIconBadgeNumber = 1;
+localNotification.hasAction = true;
+localNotification.alertAction = NotificationConfig.NoticeTitle;
+if(isRepeatDay)
+{
+//是否每天定期循环
+localNotification.repeatCalendar = UnityEngine.iOS.CalendarIdentifier.ChineseCalendar;
+localNotification.repeatInterval = UnityEngine.iOS.CalendarUnit.Day;
+}
+localNotification.soundName = UnityEngine.iOS.LocalNotification.defaultSoundName;
+UnityEngine.iOS.NotificationServices.ScheduleLocalNotification(localNotification);
+}
+}
+
+void Awake()
+{
+UnityEngine.iOS.NotificationServices.RegisterForNotifications (
+NotificationType.Alert |
+NotificationType.Badge |
+NotificationType.Sound);
+//第一次进入游戏的时候清空，有可能用户自己把游戏冲后台杀死，这里强制清空
+CleanNotification();
+}
+
+void OnApplicationPause(bool paused)
+{
+//程序进入后台时
+if(paused)
+{
+NotificationMessage(NotificationConfig.day3_text,System.DateTime.Now.AddSeconds(NotificationConfig.day3_time),false);
+NotificationMessage(NotificationConfig.day7_text,System.DateTime.Now.AddSeconds(NotificationConfig.day7_time),false);
+NotificationMessage(NotificationConfig.lift_full,System.DateTime.Now.AddSeconds(NotificationConfig.life_full_time),false);
+NotificationMessage(NotificationConfig.next_day_text,System.DateTime.Now.AddSeconds(NotificationConfig.next_day_time),false);
+//每天中午12点推送
+//NotificationMessage("雨松MOMO : 每天中午12点推送",12,true);
+}
+else
+{
+//程序从后台进入前台时
+CleanNotification();
+}
+}
+
+//清空所有本地消息
+void CleanNotification()
+{
+UnityEngine.iOS.LocalNotification l = new UnityEngine.iOS.LocalNotification (); 
+l.applicationIconBadgeNumber = -1; 
+UnityEngine.iOS.NotificationServices.PresentLocalNotificationNow (l); 
+UnityEngine.iOS.NotificationServices.CancelAllLocalNotifications (); 
+UnityEngine.iOS.NotificationServices.ClearLocalNotifications (); 
+}
+}
 #endif
