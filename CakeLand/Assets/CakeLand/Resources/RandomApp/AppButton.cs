71d575ba0016291f5acc4c9e24d1d0121626545c
CodeF <funcodegroup@gmail.com>
2017-11-28 00:31:08 +0800

    提交Random app push

---
+++ CakeLand/Assets/CakeLand/Resources/RandomApp/AppButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Umeng;
+
+public class AppButton : MonoBehaviour, IPointerClickHandler
+{
+    public RawImage img;
+    public Text text;
+    private AppInfo _info;
+    private const string MORE_GAMES = "MoreGames";
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void SetInfo(AppInfo info)
+    {
+        _info = info;
+        text.text = info.name;
+        //Texture texture = Resources.Load(info.icon + ".png") as Texture;
+        Object obj = Resources.Load(info.icon);
+        obj = Resources.Load(info.icon, typeof(Texture));
+        Texture texture = obj as Texture;
+        img.texture = texture;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        #if UNITY_ANDROID
+                Application.OpenURL(_info.aaddres);
+        #elif UNITY_IPHONE
+                Application.OpenURL(_info.iaddres);
+        #endif
+                //此处增加点击评论游戏按钮
+                GA.Event(MORE_GAMES, _info.name);
+            }
+}
