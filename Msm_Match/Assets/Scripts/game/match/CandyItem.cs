7f2d89111d8755fdb5bd6dacb449cc4cce56e95c
mashaomin <m623606643@163.com>
2017-12-05 17:34:51 +0800

    新增加插件

---
+++ Msm_Match/Assets/Scripts/game/match/CandyItem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Summer.Game
+{
+    public class CandyItem : MonoBehaviour
+    {
+        public CandyInfo _info;
+
+        #region Unity内置函数 MonoBehaviour
+
+        private void Awake()
+        {
+
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        #endregion
+
+
+        public void SetInfo(CandyInfo info)
+        {
+            _info = info;
+        }
+    }
+}
