f32e22ee00df7b21db7937ef102e61064abc88b2
CodeF <funcodegroup@gmail.com>
2017-12-10 19:14:45 +0800

    修改iap配置

--- IceBlast/Assets/Plugins/UnityPurchasing/generated/GooglePlayTangle.cs
+++ IceBlast/Assets/Plugins/UnityPurchasing/generated/GooglePlayTangle.cs
@@ -4,9 +4,9 @@
 namespace UnityEngine.Purchasing.Security {
     public class GooglePlayTangle
     {
-        private static byte[] data = System.Convert.FromBase64String("agm6DH+MbWar7QkZCjcGo9u33eVVKhx4XySMxXR9FVSKFwvZ7UE+G8lvx+Uj5GYsuwVlk7S+gjONZcAhSlsL4YLmdloI3182mzK8Nc/GwyEpMIm7m8ZSf31+tjhkLPRyZEhcQ9SFdFnzyBDqsIqnNAD2WZK4tmdBXxEFh2IhtRZX/rh3F1Yg3zvSPn5DNKn4VPA4XkxVetpnv0qgheHt9TiKCSo4BQ4BIo5Ajv8FCQkJDQgLsEC81l26uJEEzLrj8ZHucaqNjcGghXEYGS7F5WpGp0QsZRw9mAoEw1rRNShdRRwCC7/nsmZVvV/ugTT59terlJYI4l86KsINOx4KGqNACfOKCQcIOIoJAgqKCQkI4S0Mtu/+zNeLFOJkg1IScwoLCQgJ");
-        private static int[] order = new int[] { 13,7,5,5,4,6,8,12,13,11,13,13,13,13,14 };
-        private static int key = 8;
+        private static byte[] data = System.Convert.FromBase64String("rbQNPx9C1vv5+jK84Khw9uDM2MckAfWcnapBYe7CI8Co4Zi5HI6ARw6Ng4y8Do2Gjg6NjYxlqYgya3pIx7AtfNB0vNrI0f5e4zvOJAFlaXFyUy8QEoxm276uRom/mo6eJ8SNd7wOja68gYqFpgrECnuBjY2NiYyPzt+PZQZi8t6MW9uyH7Y4sUtCR6XujT6I+wjp4i9pjZ2Os4InXzNZYTTEOFLZPjwVgEg+Z3UVavUuCQlFUAHw3XdMlG40DiOwhHLdFjwy48XRrpj826AIQfD5kdAOk49dacW6n95VsazZwZiGjztjNuLROdtqBbB9TetDYadg4qg/geEXMDoGtwnhRKXblYED5qUxktN6PPOT0qRbv1a6+lMPkGbgB9aW946PjYyN");
+        private static int[] order = new int[] { 4,3,3,12,9,9,12,13,11,12,13,13,12,13,14 };
+        private static int key = 140;
 
         public static readonly bool IsPopulated = true;
 
