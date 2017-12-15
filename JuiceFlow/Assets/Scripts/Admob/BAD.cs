dea526f28714b97e8bccb6bafcb8066b2934d22e
CodeF <funcodegroup@gmail.com>
2017-12-15 22:35:52 +0800

    调整banner广告

--- JuiceFlow/Assets/Scripts/Admob/BAD.cs
+++ JuiceFlow/Assets/Scripts/Admob/BAD.cs
@@ -2,40 +2,45 @@
 using System.Collections;
 using GoogleMobileAds.Api;
 
-public class BAD : MonoBehaviour {
-    #if UNITY_ANDROID
+public class BAD : MonoBehaviour
+{
+#if UNITY_ANDROID
     string adUnitId = "ca-app-pub-4966484504552142/5192141841";
-    #elif UNITY_IPHONE
+#elif UNITY_IPHONE
     string adUnitId = "ca-app-pub-4966484504552142/8556671781";
-    #else
+#else
     string adUnitId = "unexpected_platform";
-    #endif
-	
-	public GameObject BUI;
+#endif
+
+    public GameObject BUI;
     private BannerView bannerView;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         float num = (float)Screen.height / Screen.width;
+
+        int num2 = PlayerPrefs.GetInt("AdmobInterstitial",0);
+        //if (result > 0) return;
         //Debug.Log("size=" + num);
-        if (num > 1.70f)
+        if (num > 1.70f && num2 == 0)
         {
+            UIChange();
             Showbanner();
         }
-	}
+    }
 
-    private void Showbanner() {
+    private void Showbanner()
+    {
         //Debug.Log("showbanner");
         bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
         AdRequest request = new AdRequest.Builder().Build();
-
-        UIChange();
-
         bannerView.LoadAd(request);
     }
 
-    private void UIChange() {
+    private void UIChange()
+    {
         //BUI.transform.position = new Vector3(0,28.6f,0);
-		BUI.transform.Translate(new Vector3(0, 1.15f, 0));
+        BUI.transform.Translate(new Vector3(0, 1.15f, 0));
     }
-}	
+}
