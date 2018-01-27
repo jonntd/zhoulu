49bc1f6235a9dc1624925ba0a5a3bb9cbe929475
CodeF <funcodegroup@gmail.com>
2018-01-27 20:20:24 +0800

    修复firebase

---
+++ JuiceFlow/Assets/Scripts/FirebaseManager.cs
@@ -0,0 +1,67 @@
+﻿// Copyright 2016 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Firebase;
+using Firebase.Analytics;
+
+// Handler for UI buttons on the scene.  Also performs some
+// necessary setup (initializing the firebase app, etc) on
+// startup.
+public class FirebaseManager : MonoBehaviour
+{
+    DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+    #region ===字段===
+    #endregion
+    // When the app starts, check to make sure that we have
+    // the required dependencies to use Firebase, and if not,
+    // add them if possible.
+    public virtual void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                InitializeFirebase();
+            }
+            else {
+                Debug.LogError(
+                  "Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
+
+    // Handle initialization of the necessary firebase modules:
+    void InitializeFirebase()
+    {
+        Debug.Log("Enabling data collection.");
+        FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        // Debug.Log("Set user properties.");
+        // Set the user's sign up method.
+        // FirebaseAnalytics.SetUserProperty(FirebaseAnalytics.UserPropertySignUpMethod,"Google");
+        // Set the user ID.
+        // FirebaseAnalytics.SetUserId("uber_user_510");
+    }
+
+    // End our analytics session when the program exits.
+    void OnDestroy() { }
+    #region ===属性===
+    #endregion
+    
+}
